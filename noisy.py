ba1086e0bae903dfe4f995b8775485a8b7d48511
madereddy <49539048+madereddy@users.noreply.github.com>
2023-09-07 10:08:30 -0400

    Update noisy.py
    Fix urllib parse import

--- noisy.py
+++ noisy.py
@@ -13,7 +13,7 @@ import fake_useragent
 from fake_useragent import UserAgent
 ua = UserAgent(min_percentage=15.1)
 
-from urlparse import urljoin, urlparse
+from urllib.parse import urljoin, urlparse
 
 class Crawler(object):
     def __init__(self):
